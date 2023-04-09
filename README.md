0efaa4ca2540c639259586f752a7881d130eb7b0
Dave Thomas <david.thomas@daves-mbp-2.lan>
2023-04-08 18:40:45 -0700

    first tested version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ghstats
 Grab statistics from GitHub
+
+# Install
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+# Settings
+.env file should have
+GITHUB_API_TOKEN=
+REPO_OWNER=my_orgname
+REPO_NAMES=repo1,repo2,repo3
+DEFAULT_MONTHS_LOOKBACK=3
--- ghstats.py
+++ ghstats.py
@@ -1,65 +1,94 @@
 import os
+from datetime import datetime, timedelta
+from typing import List, Tuple
+import pandas as pd
+import numpy as np
+
 import requests
-import datetime
 
-# Load environment variables
-from dotenv import load_dotenv
-load_dotenv()
+GITHUB_API_BASE_URL = "https://api.github.com"
+DEFAULT_MONTHS_LOOKBACK = int(os.getenv("DEFAULT_MONTHS_LOOKBACK", 0))
+API_TOKEN = os.getenv("GITHUB_API_TOKEN")
 
-# Define API endpoints
-base_url = "https://api.github.com"
-repos_endpoint = "/repos/{owner}/{repo}"
-commits_endpoint = "/repos/{owner}/{repo}/commits"
-pulls_endpoint = "/repos/{owner}/{repo}/pulls"
+def get_prs_for_contributor(repo_owner: str, repo_name: str, contributor: str, months_lookback: int):
+    access_token = API_TOKEN
+    default_lookback = int(os.getenv('DEFAULT_MONTHS_LOOKBACK', 3))
+    since_date = (datetime.now() - timedelta(weeks=default_lookback*4)).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-# Define date range for statistics calculation
-start_date = datetime.date(2022, 1, 1)
-end_date = datetime.date(2022, 12, 31)
+    url = f'https://api.github.com/search/issues?q=type:pr+repo:{repo_owner}/{repo_name}+author:{contributor}+created:>{since_date}&per_page=100'
+    headers = {'Authorization': f'token {access_token}'}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return 0
+    # response.raise_for_status()
+    else:
+        return response.json()['total_count']
 
-# Define function to calculate number of workdays between two dates
-def workdays(start_date, end_date):
-    days = (end_date - start_date).days + 1
-    return sum(1 for day in range(days) if datetime.date.weekday(start_date + datetime.timedelta(day)) < 5)
+def get_workdays(start_date, end_date):
+    dates = pd.date_range(start_date, end_date, freq='B')
+    dates = dates.values.astype('datetime64[D]')  # cast to datetime64[D]
+    mask = np.is_busday(dates)
+    num_workdays = np.count_nonzero(mask)
+    return num_workdays
 
-# Define function to calculate statistics per contributor per repo
-def calculate_statistics(users, repos):
-    # Initialize dictionary to store statistics
-    statistics = {}
+def get_contributors_stats(repo_owner: str, repo_names: List[str], months_lookback: int) -> List[Tuple[str, str, str, float, float, float, float]]:
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}",
+        "Accept": "application/vnd.github+json",
+    }
 
-    # Loop through each user and repo combination
-    for user in users:
-        for repo in repos:
-            # Make API requests to retrieve data
-            repo_url = base_url + repos_endpoint.format(owner=user, repo=repo)
-            repo_data = requests.get(repo_url, headers={"Authorization": f"token {os.environ.get('GITHUB_API_TOKEN')}"}).json()
-            commits_url = base_url + commits_endpoint.format(owner=user, repo=repo)
-            commits_data = requests.get(commits_url, headers={"Authorization": f"token {os.environ.get('GITHUB_API_TOKEN')}"}).json()
-            pulls_url = base_url + pulls_endpoint.format(owner=user, repo=repo)
-            pulls_data = requests.get(pulls_url, headers={"Authorization": f"token {os.environ.get('GITHUB_API_TOKEN')}"}).json()
+    contributors_stats = []
+    today: datetime = datetime.today()
+    start_date = pd.Timestamp.today() - pd.DateOffset(months=DEFAULT_MONTHS_LOOKBACK)
+    end_date = pd.Timestamp.today()
+    num_workdays = get_workdays(start_date, end_date)
+    contributors = []
 
-            # Initialize dictionary to store statistics for current user and repo combination
-            user_stats = {
-                "commits_per_workday": 0,
-                "prs_per_workday": 0,
-                "review_comments_per_workday": 0,
-                "lines_of_code_per_workday": 0
-            }
+    for repo_name in repo_names:
 
-            # Loop through each commit and calculate statistics
-            for commit in commits_data:
-                commit_date = datetime.datetime.strptime(commit["commit"]["author"]["date"], "%Y-%m-%dT%H:%M:%SZ").date()
-                if start_date <= commit_date <= end_date:
-                    commit_workdays = workdays(start_date, commit_date)
-                    user_stats["commits_per_workday"] += 1 / commit_workdays
-                    user_stats["lines_of_code_per_workday"] += commit["stats"]["additions"] / commit_workdays
+        # Returns the total number of commits authored by the contributor. In addition, the response includes a Weekly Hash (weeks array) with the following information:
+        # w - Start of the week, given as a Unix timestamp.
+        # a - Number of additions
+        # d - Number of deletions
+        # c - Number of commits
+        url = f"{GITHUB_API_BASE_URL}/repos/{repo_owner}/{repo_name}/stats/contributors"
+        response = requests.get(url, headers=headers)
 
-            # Loop through each pull request and calculate statistics
-            for pull in pulls_data:
-                pull_date = datetime.datetime.strptime(pull["created_at"], "%Y-%m-%dT%H:%M:%SZ").date()
-                if start_date <= pull_date <= end_date:
-                    pull_workdays = workdays(start_date, pull_date)
-                    user_stats["prs_per_workday"] += 1 / pull_workdays
+        if response.status_code != 200:
+            continue
 
-                    # Make API request to retrieve review comments for pull request
-                    review_comments_url = pull["comments_url"]
-                    review_comments_data = requests.get(review_comments
+        for contributor in response.json():
+            contributor_name = contributor.get("author", {}).get("login", "")
+            contributor_stats = {"repo": repo_name, "contributor_name": contributor_name, "start_date": start_date, "end_date": today, "num_workdays": num_workdays, "commits": 0, "prs": 0, "reviews": 0, "changed_lines": 0}
+            for weekly_stat in contributor["weeks"]:
+                weekly_date = datetime.utcfromtimestamp(weekly_stat["w"])
+
+                if weekly_date >= start_date:
+                    contributor_stats["start_date"] = weekly_date
+                    contributor_stats["commits"] += weekly_stat["c"]
+                    contributor_stats["reviews"] += weekly_stat.get("r", 0)
+                    contributor_stats["changed_lines"] += weekly_stat.get("d", 0) + weekly_stat.get("a", 0)
+
+            contributor_stats["prs"] = get_prs_for_contributor(repo_owner,repo_name, contributor_name,months_lookback)
+            # Only save stats if there are stats to save
+            if contributor_stats["commits"] == 0 and contributor_stats["prs"] == 0 and contributor_stats["reviews"] == 0 and contributor_stats["changed_lines"] == 0:
+                continue
+        
+            contributors.append(contributor_stats)
+
+    return contributors
+
+def save_contributors_to_csv(contributors, filename):
+    df = pd.DataFrame(contributors)
+    df.to_csv(filename, index=False)
+
+if __name__ == "__main__":
+    repo_owner = os.getenv("REPO_OWNER")
+    repo_names = os.getenv("REPO_NAMES").split(",")
+    months_lookback = int(os.getenv("DEFAULT_MONTHS_LOOKBACK", 3))
+
+    contributors_stats = get_contributors_stats(repo_owner, repo_names, months_lookback)
+    save_contributors_to_csv(contributors_stats, f'{repo_owner}_contributor_stats.csv')
+
+    for contributor_stat in contributors_stats:
+        print(contributor_stat)
