715ad4b37ef6e4d816727c778891a573ffadbefc
Dave Thomas <david.thomas@daves-mbp-2.lan>
2023-04-09 13:38:35 -0700

    added names and normalizing for business days. Missing - start date as a collaborator, which may be more recent than lookback date

--- ghstats.py
+++ ghstats.py
@@ -3,7 +3,7 @@ from datetime import date, datetime, timedelta
 from typing import List, Tuple
 import pandas as pd
 import numpy as np
-
+import time
 import requests
 
 GITHUB_API_BASE_URL = "https://api.github.com"
@@ -12,6 +12,37 @@ API_TOKEN = os.getenv("GITHUB_API_TOKEN")
 
 import requests
 import datetime
+
+import requests
+import json
+
+def get_github_collaborator_name(username):
+    """
+    Returns the name of a collaborator on a GitHub repository, given their username.
+    
+    Parameters:
+    owner (str): The username of the owner of the repository.
+    repo (str): The name of the repository.
+    username (str): The username of the collaborator whose name to retrieve.
+    access_token (str): A personal access token with appropriate permissions to access the collaborator's information.
+    
+    Returns:
+    str: The name of the collaborator, or None if the collaborator is not found or there is an error retrieving their information.
+    """
+    url = f"https://api.github.com/users/{username}"
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        collaborator_info = json.loads(response.text)
+        collaborator_name = collaborator_info['name']
+        return collaborator_name
+    else:
+        return None
+
 
 def get_commenters_stats(repo_owner, repo_name, months_lookback):
     # calculate start and end dates
@@ -52,15 +83,17 @@ def get_commenters_stats(repo_owner, repo_name, months_lookback):
 def get_prs_for_contributor(repo_owner: str, repo_name: str, contributor: str):
     access_token = API_TOKEN
     default_lookback = int(os.getenv('DEFAULT_MONTHS_LOOKBACK', 3))
-    since_date = (datetime.now() - timedelta(weeks=default_lookback*4)).strftime('%Y-%m-%dT%H:%M:%SZ')
+    since_date = (datetime.datetime.now() - timedelta(weeks=default_lookback*4)).strftime('%Y-%m-%dT%H:%M:%SZ')
 
     url = f'https://api.github.com/search/issues?q=type:pr+repo:{repo_owner}/{repo_name}+author:{contributor}+created:>{since_date}&per_page=1000'
     headers = {'Authorization': f'token {access_token}'}
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        return 0
+        if response.status_code == 422: # Try again
+            time.sleep(2000)
+            response = requests.get(url, headers=headers)
     # response.raise_for_status()
-    else:
+    if response.status_code == 200:
         return response.json()['total_count']
 
 def get_workdays(start_date, end_date):
@@ -93,29 +126,48 @@ def get_contributors_stats(repo_owner: str, repo_names: List[str], months_lookba
         response = requests.get(url, headers=headers)
 
         if response.status_code != 200:
-            continue
+            if response.status_code == 202:
+                retry_url = response.headers.get('Location')
+                if retry_url:
+                    # Wait for 5 seconds before checking the status
+                    time.sleep(5)
+                    response = requests.get(retry_url, headers=headers)
+                    # Handle the status response
+                else:
+                    # Wait for 5 seconds before retrying the request
+                    time.sleep(5)
+                    response = requests.get(url, headers=headers)
+        # Handle the response
+        if response.status_code == 200:
+            for contributor in response.json():
+                contributor_username = contributor.get("author", {}).get("login", "")
+                contributor_name = get_github_collaborator_name(contributor_username)
+                contributor_stats = {"repo": repo_name, "contributor_name": contributor_name, "contributor_username": contributor_username, "start_date": start_date, "end_date": today, "num_workdays": num_workdays, "commits": 0, "prs": 0, "review_comments": 0, "changed_lines": 0}
+                for weekly_stat in contributor["weeks"]:
+                    weekly_date = datetime.datetime.utcfromtimestamp(weekly_stat["w"])
 
-        for contributor in response.json():
-            contributor_name = contributor.get("author", {}).get("login", "")
-            contributor_stats = {"repo": repo_name, "contributor_name": contributor_name, "start_date": start_date, "end_date": today, "num_workdays": num_workdays, "commits": 0, "prs": 0, "review_comments": 0, "changed_lines": 0}
-            for weekly_stat in contributor["weeks"]:
-                weekly_date = datetime.utcfromtimestamp(weekly_stat["w"])
+                    if weekly_date >= start_date:
+                        contributor_stats["start_date"] = weekly_date
+                        contributor_stats["commits"] += weekly_stat["c"]
+                        contributor_stats["changed_lines"] += weekly_stat.get("d", 0) + weekly_stat.get("a", 0)
 
-                if weekly_date >= start_date:
-                    contributor_stats["start_date"] = weekly_date
-                    contributor_stats["commits"] += weekly_stat["c"]
-                    contributor_stats["changed_lines"] += weekly_stat.get("d", 0) + weekly_stat.get("a", 0)
+                contributor_stats["prs"] = get_prs_for_contributor(repo_owner,repo_name, contributor_username)
+                for dict_commenter_stats in list_dict_commenter_stats:
+                    if dict_commenter_stats["commenter_name"] == contributor_username:
+                        contributor_stats["review_comments"] += dict_commenter_stats["num_comments"]
 
-            contributor_stats["prs"] = get_prs_for_contributor(repo_owner,repo_name, contributor_name,months_lookback)
-            for dict_commenter_stats in list_dict_commenter_stats:
-                if dict_commenter_stats["commenter_name"] == contributor_name:
-                    contributor_stats["review_comments"] += dict_commenter_stats["num_comments"]
+                # Only save stats if there are stats to save
+                if contributor_stats["commits"] == 0 and contributor_stats["prs"] == 0 and contributor_stats["review_comments"] == 0 and contributor_stats["changed_lines"] == 0:
+                    continue
 
-            # Only save stats if there are stats to save
-            if contributor_stats["commits"] == 0 and contributor_stats["prs"] == 0 and contributor_stats["reviews"] == 0 and contributor_stats["changed_lines"] == 0:
-                continue
-        
-            contributors.append(contributor_stats)
+                # Normalize per workday in lookback period
+                else:
+                    contributor_stats["commits"] /= num_workdays
+                    contributor_stats["changed_lines"] /= num_workdays
+                    contributor_stats["prs"] /= num_workdays
+                    contributor_stats["review_comments"] /= num_workdays
+            
+                contributors.append(contributor_stats)
 
     return contributors
 
