e3bf6ef577fdfc1b804ed172d465bed890052518
Dave Thomas <davidcthomas@gmail.com>
2024-02-20 15:25:50 -0800

    updating Hex files. Note these are intended to be used within a Hex environment and are only needed for visualization.

--- ghs_hex_inputs.py
+++ ghs_hex_inputs.py
@@ -1,3 +1,8 @@
+"""
+NOTE: This code WILL NOT work outside of a Hex environment. It is designed to be used within the Hex ecosystem.
+You must have a Hex account and be logged in to run this code, along with a valid datasource set up to use the data.
+See the dbschema.sql for setting up a Snowflake datasource in Hex here: https://hex.tech/docs/snowflake
+"""
 import pandas as pd
 import datetime
 from dateutil.relativedelta import relativedelta
@@ -16,9 +21,11 @@ def get_first_day_of_month_one_year_ago() -> datetime.date:
     return first_day_of_month_one_year_ago
 
 
-def present_input_selections(repo_aggregated: bool, repo_topic_aggregated: bool, start_date: datetime.date, multiselect_contributors, multiselect_repo_names, multiselect_repo_topics) -> dict:
-
+def present_input_selections(metric_selector, repo_aggregated: bool, repo_topic_aggregated: bool, start_date: datetime.date, multiselect_contributors, multiselect_repo_names, multiselect_repo_topics) -> dict:
+    default_metric_selector: list = [
+        "prs_per_day", "avg_pr_duration", "avg_code_movement_per_pr"]
     dict_inputs: dict = {
+        "metric_selector": metric_selector,
         "multiselect_contributors": multiselect_contributors,
         "repo_aggregated": repo_aggregated,
         "repo_topic_aggregated": repo_topic_aggregated,
@@ -26,6 +33,9 @@ def present_input_selections(repo_aggregated: bool, repo_topic_aggregated: bool,
         "multiselect_repo_topics": multiselect_repo_topics,
         "start_date": start_date
     }
+    # Default metric_selector
+    if len(metric_selector) == 0:
+        dict_inputs["metric_selector"] = default_metric_selector
 
     # Calculate the default start_date if not provided
     if start_date is None:
@@ -46,7 +56,8 @@ def present_input_selections(repo_aggregated: bool, repo_topic_aggregated: bool,
             print("Repo topic aggregation is selected, overriding repo aggregation and nullifying contributors selection.")
 
     # Display the explanation and values to be used
-    print("Creating Hex plot output using the following values:")
+    print(
+        f"Creating Hex plot output using the following metrics: {dict_inputs['metric_selector']}")
     print(f"Date range of analysis period: {start_date} to the present")
 
     if dict_inputs["repo_topic_aggregated"]:
@@ -63,6 +74,6 @@ def present_input_selections(repo_aggregated: bool, repo_topic_aggregated: bool,
 
 
 # Call the function to display the values to be used
-gdict_inputs: dict = present_input_selections(
-    repo_aggregated, repo_topic_aggregated, start_date, multiselect_contributors, multiselect_repo_names, multiselect_repo_topics)
+gdict_inputs: dict = present_input_selections(metric_selector, repo_aggregated, repo_topic_aggregated,
+                                              start_date, multiselect_contributors, multiselect_repo_names, multiselect_repo_topics)
 start_date_date = gdict_inputs["start_date"]
--- ghs_hex_plot.py
+++ ghs_hex_plot.py
@@ -1,3 +1,9 @@
+"""
+NOTE: This code WILL NOT work outside of a Hex environment. It is designed to be used within the Hex ecosystem.
+You must have a Hex account and be logged in to run this code, along with a valid datasource set up to use the data.
+See the dbschema.sql for setting up a Snowflake datasource in Hex here: https://hex.tech/docs/snowflake
+"""
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,64 +17,146 @@ hex_snowflake_conn = hextoolkit.get_data_connection('Contributor Stats')
 session = hex_snowflake_conn.get_snowpark_session()
 
 
-def create_and_process_dataframe(dict_inputs: dict, selected_metric: str) -> pd.DataFrame:
-
-    repo_aggregated_view: bool = dict_inputs.get("repo_aggregated", False)
-    repo_topic_aggregated_view: bool = dict_inputs.get(
+def create_and_process_dataframe(dict_inputs: dict, selected_metric: str) -> dict:
+    """ 
+    Returns a dict of {"df": the plottable dataset , "contributors_list": a list of contributors, if filtered in selectors}
+    """
+    dict_results: dict = {"df": None, "contributors_list": None}
+    # Extracting necessary parameters from dict_inputs
+    repo_aggregated_view = dict_inputs.get("repo_aggregated", False)
+    repo_topic_aggregated_view = dict_inputs.get(
         "repo_topic_aggregated", False)
-    username_filter: list = dict_inputs["multiselect_contributors"]
-    repo_filter: list = dict_inputs.get("multiselect_repo_names", [])
-    topic_filter: list = dict_inputs.get("multiselect_repo_topics", [])
-    start_date_date: datetime.date = dict_inputs.get(
-        "start_date", get_first_day_of_month_one_year_ago())
-    # for modifying SQL
+    username_filter = dict_inputs.get("multiselect_contributors", [])
+    repo_filter = dict_inputs.get("multiselect_repo_names", [])
+    topic_filter = dict_inputs.get("multiselect_repo_topics", [])
+    start_date_date = dict_inputs.get(
+        "start_date", datetime.date.today() - relativedelta(years=1))
+
     start_date_str = start_date_date.strftime('%Y-%m-%d')
 
+    # Adjust the query based on filters applied
+    contributors: str = ""
+    query_conditions = []
+    if 'multiselect_contributors' in dict_inputs and dict_inputs['multiselect_contributors']:
+        contributors = "', '".join(dict_inputs['multiselect_contributors'])
+        query_conditions.append(
+            f'cs."contributor_name" IN (\'{contributors}\')')
+    if 'multiselect_repo_names' in dict_inputs and dict_inputs['multiselect_repo_names']:
+        repos = "', '".join(dict_inputs['multiselect_repo_names'])
+        query_conditions.append(f'cs."repo" IN (\'{repos}\')')
+    if 'multiselect_repo_topics' in dict_inputs and dict_inputs['multiselect_repo_topics']:
+        topics = "', '".join(dict_inputs['multiselect_repo_topics'])
+        query_conditions.append(f'rt."repo_topic" IN (\'{topics}\')')
+
+    conditions_str = " AND ".join(
+        query_conditions) if query_conditions else "1=1"
+
     query_template = f"""
-    SELECT cs."repo", cs."stats_beginning", cs."contributor_name", cs."{selected_metric}", rt."repo_topic"
+    SELECT 
+        cs."repo", 
+        cs."stats_beginning", 
+        cs."contributor_name", 
+        cs."{selected_metric}" AS "selected_metric",
+        rt."repo_topic",
+        cs."changed_lines_per_day",
+        cs."commits_per_day",
+        cs."review_comments_per_day",
+        cs."prs_per_day", 
+        cs."avg_pr_duration", 
+        cs."avg_code_movement_per_pr"
     FROM "contributor_stats" cs
     LEFT JOIN "repo_topics" rt ON cs."repo" = rt."repo_name"
-    WHERE cs."stats_beginning" >= '{start_date_str}'
+    WHERE cs."stats_beginning" >= '{start_date_str}' AND ({conditions_str})
     ORDER BY cs."stats_beginning" ASC, cs."repo";
     """
 
     # Execute the query and load results into a pandas DataFrame
-    df: pd.DataFrame = session.sql(query_template).to_pandas()
-    # Ensure 'stats_beginning' is a datetime format
+    df = session.sql(query_template).to_pandas()
     df['stats_beginning'] = pd.to_datetime(df['stats_beginning'])
 
-    # Repo topic aggregate: also count the number of contributors and repos
+    # Capture the contributor names before aggregation if username_filter is not empty
+    if len(contributors) > 0:
+        contributors_list = df[df['contributor_name'].isin(
+            username_filter)]['contributor_name'].unique()
+    else:
+        contributors_list = []
+
+    dict_results["contributors_list"] = contributors_list
+
+    # Aggregation and calculation of repo_count for repo_topic_aggregated_view
     if repo_topic_aggregated_view:
-        df = df.groupby(['repo_topic', 'stats_beginning']).agg(
-            avg_selected_metric=(selected_metric, 'mean'),
-            contributor_count=('contributor_name', 'nunique'),
-            repo_count=('repo', 'nunique')).reset_index()
+        # Aggregate data
+        agg_dict = {
+            selected_metric: 'mean',  # Average of the selected metric
+            'contributor_name': 'nunique',  # Count unique contributors
+            'repo': 'nunique',  # Count unique repos
+            # Sum of per day metrics
+            'commits_per_day': 'sum',
+            'prs_per_day': 'sum',
+            'changed_lines_per_day': 'sum',
+            'review_comments_per_day': 'sum',
+            'avg_pr_duration': 'mean',
+            'avg_code_movement_per_pr': 'mean'
+        }
+
+        df_agg = df.groupby(['repo_topic', 'stats_beginning']).agg(
+            agg_dict).reset_index()
+
+        # Calculate per contributor metrics
+        for metric in ['commits_per_day', 'prs_per_day', 'changed_lines_per_day', 'review_comments_per_day']:
+            per_contributor_metric = f"{metric}_per_contributor"
+            df_agg[per_contributor_metric] = df_agg[metric] / \
+                df_agg['contributor_name']
+
+        df = df_agg.rename(
+            columns={'contributor_name': 'contributor_count', 'repo': 'repo_count'})
 
     elif repo_aggregated_view:
-        # Group by 'repo' and 'stats_beginning', then aggregate
-        df = df.groupby(['repo', 'stats_beginning']).agg(
-            avg_selected_metric=(selected_metric, 'mean'),
-            contributor_count=('contributor_name', 'nunique')).reset_index()
-        df = df.drop_duplicates(
-            subset=['repo', 'stats_beginning'], keep='first')
+        # Define aggregation dictionary
+        agg_dict = {
+            selected_metric: 'mean',  # Average of the selected metric
+            'contributor_name': 'nunique',  # Count unique contributors
+            # Sum of per day metrics
+            'commits_per_day': 'sum',
+            'prs_per_day': 'sum',
+            'changed_lines_per_day': 'sum',
+            'review_comments_per_day': 'sum',
+            'avg_pr_duration': 'mean',
+            'avg_code_movement_per_pr': 'mean'
+        }
 
-    # Remove duplicates if no aggregation is applied
+        # Perform aggregation
+        df_agg = df.groupby(['repo', 'stats_beginning']
+                            ).agg(agg_dict).reset_index()
+
+        # Rename 'contributor_name' column to 'contributor_count' for clarity
+        df_agg.rename(
+            columns={'contributor_name': 'contributor_count'}, inplace=True)
+
+        # Calculate "per contributor" metrics for the aggregated data
+        for metric in ['commits_per_day', 'prs_per_day', "changed_lines_per_day", "review_comments_per_day"]:
+            per_contributor_metric = f"{metric}_per_contributor"
+            df_agg[per_contributor_metric] = df_agg[metric] / \
+                df_agg['contributor_count']
+
+        # Assign the aggregated DataFrame back to df
+        df = df_agg
+
+    # Handling duplicates and filtering based on the initial configuration
     if not repo_aggregated_view and not repo_topic_aggregated_view:
         df = df.drop_duplicates(
             subset=['contributor_name', 'repo', 'stats_beginning'], keep='first')
 
-    # Filter DataFrame
-    if 'contributor_name' in df.columns and username_filter and (repo_aggregated_view is False and repo_topic_aggregated_view is False):
-        df = df[df['contributor_name'].isin(username_filter)]
-    if 'repo' in df.columns and repo_filter:
-        df = df[df['repo'].isin(repo_filter)]
-    if 'repo_topic' in df.columns and topic_filter:
-        df = df[df['repo_topic'].isin(topic_filter)]
+    dict_results["df"] = df
 
-    return df
+    return dict_results
 
 
-def plot_data(df, dict_inputs: dict, selected_metric: str):
+def plot_data(dict_data: dict, dict_inputs: dict, selected_metric: str):
+    df: pd.DataFrame = dict_data.get("df")
+    contributors_list: list = dict_data.get("contributors_list", [])
+    title: str = dict_data.get("title", "Productivity")
+
     start_date_str: str = dict_inputs["start_date"].strftime('%Y-%m-%d')
     repo_aggregated_view: bool = dict_inputs.get("repo_aggregated", False)
     repo_topic_aggregated_view: bool = dict_inputs.get(
@@ -77,19 +165,29 @@ def plot_data(df, dict_inputs: dict, selected_metric: str):
     # Initialize the figure and axes
     fig, ax = plt.subplots(figsize=(12, 6))
 
-    if repo_topic_aggregated_view:
-        # When data is aggregated by repo_topic
-        for topic, group in df.groupby('repo_topic'):
-            ax.plot(group['stats_beginning'], group['avg_selected_metric'], marker='', linewidth=2,
-                    label=f"{topic} - Contributors: {group['contributor_count'].iloc[0]}, Repos: {group['repo_count'].iloc[0]}")
-        legend_title = 'Repo Topic'
-    elif repo_aggregated_view:
-        # When data is aggregated by repo
-        for (repo, group) in df.groupby('repo'):
-            ax.plot(group['stats_beginning'], group['avg_selected_metric'], marker='',
-                    linewidth=2, label=f"{repo} (Contributors: {group['contributor_count'].iloc[0]})")
+    if repo_topic_aggregated_view or repo_aggregated_view:
+        # Determine if "per contributor" metrics should be plotted
+        per_contributor_metric = f"{selected_metric}_per_contributor"
+        if per_contributor_metric in df.columns:
+            plot_metric = per_contributor_metric
+            label_suffix = " per contributor"
+        else:
+            plot_metric = selected_metric
+            label_suffix = ""
 
-        legend_title = 'Repo'
+        if repo_topic_aggregated_view:
+            # When data is aggregated by repo_topic
+            for topic, group in df.groupby('repo_topic'):
+                ax.plot(group['stats_beginning'], group[plot_metric], marker='', linewidth=2,
+                        label=f"{topic}{label_suffix} - Contributors: {group['contributor_count'].iloc[0]}, Repos: {group['repo_count'].iloc[0]}")
+            legend_title = 'Repo Topic'
+        elif repo_aggregated_view:
+            # When data is aggregated by repo
+            for (repo, group) in df.groupby('repo'):
+                ax.plot(group['stats_beginning'], group[plot_metric], marker='', linewidth=2,
+                        label=f"{repo} (Contributors: {group['contributor_count'].iloc[0]})")
+
+            legend_title = 'Repo'
     else:
         # When showing individual contributor data
         for name, group in df.groupby('contributor_name'):
@@ -97,16 +195,12 @@ def plot_data(df, dict_inputs: dict, selected_metric: str):
                     marker='', linewidth=2, label=name)
         legend_title = 'Contributor Name'
 
-    # # Plotting data
-    # for name, group in df.groupby('contributor_name'):
-    #     ax.plot(group['stats_beginning'], group[f'{selected_metric}'], marker='', linewidth=2, label=name)
-
     # Setting titles and labels
-    title: str = f'{selected_metric} since {start_date_str}'
+    ytitle: str = f'{selected_metric}{label_suffix} since {start_date_str}'
 
     ax.set_title(title, color='black')  # Ensure title is visible
     ax.set_xlabel('Date', color='black')
-    ax.set_ylabel(title)
+    ax.set_ylabel(ytitle)
 
     # Date formatting
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
@@ -117,7 +211,7 @@ def plot_data(df, dict_inputs: dict, selected_metric: str):
     ax.tick_params(axis='y', colors='black')
 
     # Setting legend with explicit visibility
-    ax.legend(title='Contributor Name', bbox_to_anchor=(
+    ax.legend(title=f'{legend_title}', bbox_to_anchor=(
         1.05, 1), loc='upper left', edgecolor='black')
 
     # Explicitly set the figure and axes background color after all plotting commands
@@ -133,32 +227,46 @@ def plot_data(df, dict_inputs: dict, selected_metric: str):
     plt.tight_layout()
 
     # Display the plot
-    print(f"Plot of {title}")
+    # print(f"Plot of {title}")
     plt.show()
+    return
 
 
+# We store a list of dictionaries, including all our DFs so we can look at the table later if we want.
+df_list: list = []
 if start_date_date is not None:
-    for metric in metric_selector:
+    # list entry format is dict = {"title": str_title, "df": pd.DataFrame, "csv": str_filename}
+    for metric in gdict_inputs.get("metric_selector"):
         # Create and process the DataFrame for the current metric
-        df = create_and_process_dataframe(gdict_inputs, metric)
+        dict_results: dict = {}
+        dict_results = create_and_process_dataframe(gdict_inputs, metric)
+
+        df = dict_results.get("df", pd.DataFrame())
+        contributors_list: list = dict_results.get("contributors_list", [])
 
         # Check if DataFrame is not empty
         if not df.empty:
             # Build parts of the message conditionally using list comprehensions
             parts: list = [
-                "in GitHub repos " +
+                f"({metric}) since {start_date_date.strftime('%Y-%m-%d')}\n in GitHub repos " +
                 ", ".join(
                     multiselect_repo_names) if multiselect_repo_names else "",
-                "in GitHub repo topic " +
+                f"({metric}) since {start_date_date.strftime('%Y-%m-%d')}\n in GitHub repo topic " +
                 ", ".join(
-                    multiselect_repo_topics) if multiselect_repo_topics else ""
+                    multiselect_repo_topics) if multiselect_repo_topics else "",
+                f"\nfiltered by contributors " +
+                ", ".join(contributors_list) if len(
+                    contributors_list) > 0 else ""
             ]
 
-            # Filter out any empty strings and join the remaining parts with a space
             message = f"Productivity " + " ".join(filter(None, parts))
+            dict_results["title"] = message
+            # csv_file: str = f"{start_date_date.strftime('%Y-%m-%d')}-{message}.csv"
+            df_list.append(dict_results.copy())
+            # df.to_csv(csv_file, index=False)
             print(message)
             # Plot the data for the current metric
-            plot_data(df, gdict_inputs, metric)
+            plot_data(dict_results, gdict_inputs, metric)
         else:
             print(
                 f"No data to plot for {metric} based on the selected filters.")
